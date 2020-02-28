fe691c9910bf199ccec1137adbb45fdb160c5dde
Jo-Yewon <yeawonjo@gmail.com>
2020-02-28 23:52:14 +0900

    feat: System 함수 구현 및 포맷팅. Main.py에 Policy 추가

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-from System import SystemOriginal, SystemGA
+from SystemOriginal import SystemOriginal
+from SystemGA import SystemGA
+from SystemIGA import SystemIGA
 from Input import *
 
 
@@ -9,12 +11,17 @@ non_rt_tasks = get_non_rt_tasks()
 
 SystemOriginal(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks).run()
 
-# GA
+# GA (지연방식)
 sim_time, verbose, processor, memories = get_configuration()
 rt_tasks = get_rt_tasks()
 non_rt_tasks = get_non_rt_tasks()
 
 SystemGA(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks).run()
 
+# IGA(즉시 변경 방식)
+sim_time, verbose, processor, memories = get_configuration()
+rt_tasks = get_rt_tasks()
+non_rt_tasks = get_non_rt_tasks()
 
+SystemIGA(sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks).run()
 
--- System.py
+++ System.py
@@ -8,7 +8,6 @@ class System(metaclass=ABCMeta):
     VERBOSE_DEBUG_HARD = 2
 
     def __init__(self, sim_time, verbose, processor, memories, rt_tasks, non_rt_tasks):
-
         self.sim_time = sim_time
 
         self.verbose = verbose
@@ -26,50 +25,45 @@ class System(metaclass=ABCMeta):
         # 원소는 (다음 주기 시작시간, 태스크 인스턴스)로 담아서 주기 시작시간에 대한 우선순위 큐로 구현
         self.rt_wait_queue = []
 
+        # 그냥 리스트 (우선순위 큐 아님)
         self.non_rt_queue = []
 
         # 시작시간 오름차순 -> 매 시간마다 확인 필요. 맨앞의 태스크만 확인하면 된다.
-        self.non_rt_tasks_point = 0
+        self.non_rt_tasks_pointer = 0
 
     def print_debug(self, time):
-        #  rt_queue와 non_rt_queue 출력 코드 필요
-        temp1 = []
+        # 디버그를 위해 rt_queue와 non_rt_queue 출력
+        print("current time: {}".format(time))
+
+        temp_queue = []
         print("==========rt_queue===========")
-        while len(self.rt_queue) > 0 :
-            tup = heapq.heappop(self.rt_queue)
-            print(tup[1].desc_task())
-            temp1.append(tup)
+        while len(self.rt_queue) > 0:
+            rt_task = heapq.heappop(self.rt_queue)
+            print(rt_task.desc_task())
+            temp_queue.append(rt_task)
         print("===========the end===========")
-        heapq.heapify(temp1)
-        self.rt_queue = temp1
+        heapq.heapify(temp_queue)
+        self.rt_queue = temp_queue
 
-        temp2 = []
-        print("==========rt_queue===========")
-        while len(self.non_rt_queue) > 0 :
-            tup = heapq.heappop(self.non_rt_queue)
-            print(tup[1].desc_task())
-            temp2.append(tup)
+        print("==========non_rt_queue===========")
+        for non_rt_task in self.non_rt_tasks:
+            non_rt_task.desc_task()
         print("===========the end===========")
-        heapq.heapify(temp2)
-        self.non_rt_queue = temp2
 
+    def check_new_non_rt(self, cur_time):
+        if self.non_rt_tasks_pointer < len(self.non_rt_tasks) and self.non_rt_tasks[self.non_rt_tasks_pointer] == cur_time:
+            self.non_rt_queue.append(self.non_rt_tasks[self.non_rt_tasks_pointer])
+            self.non_rt_tasks_pointer += 1
 
-
-
-    def check_new_non_rt(self, time):
-        # TODO 새로 들어온 non_rt 태스크가 존재하는지 확인 후로, 있다 non_rt_queue에 넣어주는 코드면
-        # 동시에 여러 태스크가 들어올 수 있다는 점에도 유의, 포인터 사용 해야함.
-        pass
-
-    def check_wait_peroid_queue(self, time):
-        # rt_wait_queue에서 다음 주기의 시작을 기다리고 있는 rt_task 확인하고 새로운 주기이면 큐 옮겨주기
-        while len(self.rt_wait_queue) != 0:
-            if self.self.rt_wait_queue[0][0] > self.time:
+    def check_wait_period_queue(self, cur_time):
+        # rt_wait_queue에서 다음 주기의 시작을 기다리고 있는 rt_task 확인하고 새로운 주기가 시작된다면 큐 옮겨주기
+        new_start_rt_tasks = []
+        while len(self.rt_wait_queue) > 0:
+            if self.rt_wait_queue[0][0] > cur_time:
                 break
             rt_task = heapq.heappop(self.rt_wait_queue)[1]
-            self.push_rt_queue(rt_task)
-
-
+            new_start_rt_tasks.append(rt_task)
+        return new_start_rt_tasks
 
     def print_final_report(self):
         # TODO 최종 결과 출력하는 코드
@@ -81,15 +75,12 @@ class System(metaclass=ABCMeta):
     def push_rt_wait_queue(self, rt_task):
         heapq.heappush(self.rt_wait_queue, (rt_task.next_period_start, rt_task))
 
-    def check_rt_tasks(self, time):
+    def check_rt_tasks(self, cur_time):
         # 데드라인 넘는 태스트는 없는지 확인? 있으면 에러
         for rt_task in self.rt_tasks:
-            rt_task.is_deadline_violated()
+            rt_task.is_deadline_violated(cur_time)
 
     def setup_tasks(self):
         # 시뮬레이션 시작 전 태스크 셋팅하는 코드
-        # 모든 rt_task들을 rt_wait_queue에 넣어야 할듯.. 0 퀀텀에서 모두 주기 시작이므
         for rt_task in self.rt_tasks:
             self.push_rt_wait_queue(rt_task)
-
-
