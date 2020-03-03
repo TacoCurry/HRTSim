30c53f9de3f545a38098a013b151dd3f5bb21d31
Chatherina423 <ysh9473@naver.com>
2020-03-03 16:47:07 +0900

    added some functions about non_rt_tasks at SystemOriginal.py

--- SystemOriginal.py
+++ SystemOriginal.py
@@ -1,5 +1,5 @@
 from System import System
-
+import heapq
 
 class SystemOriginal(System):
     # 경성 태스크를 항상 오리지널 방식으로 수행하며, 비실시간이 들어오면 남는시간에 실행시켜주는 방식
@@ -19,33 +19,55 @@ class SystemOriginal(System):
             self.check_new_non_rt(cur_time)  # 새롭게 들어온 non_rt_job인 이 있는지 확인
             self.check_wait_period_queue(cur_time)  # 새롭게 주기 시작하는 job이 있는지 확인
 
-            exec_tasks = []
+            rt_exec_tasks = []
+            non_rt_exec_tasks = []
 
             if len(self.rt_queue) < self.processor.n_core:
                 # 큐에 있는 것 모두 실행가능(코어의 개수보다 적으므로)
                 for tup in self.rt_queue:
-                    exec_tasks.append(tup[1])
+                    rt_exec_tasks.append(tup[1])
                 self.rt_queue = []
 
                 if len(self.non_rt_queue) == 0:
                 # 비 실시간 없는 경우: self.processor.n_core - len(self.queue)개의 코어는 idle로 실행
                     for i in range(self.processor.n_core - len(self.rt_queue)):
-                        self.CPU.exec_idle(time=1)
+                        self.exec_idle_without_dvfs(1)
                 else:
                     # 비실시간 있는 경우
                     for i in range(self.processor.n_core - len(self.rt_queue)):
                        # 비실시간 오리지널로 수행
-                    pass
+                        for tup in self.non_rt_queue:
+                           non_rt_exec_tasks.append(tup[1])
+                        self.non_rt_queue = []
+
+                        for non_rt_exec_task in self.non_rt_queue:
+                            non_rt_exec_task.exec_active(self.processor, self.memories)
+
+                       # for other idle tasks (전력 소모 계산 및 1초 흐르기)
+                        for i in range(len(self.non_rt_queue)):
+                            task = self.non_rt_queue[i][1]
+                            task.exec_active(self.processor, self.memories)
+
+
 
             else:
-                for i in range(self.CPU.n_core):
-                    exec_tasks.append(self.pop_queue())
+                for i in range(self.processor.n_core):
+                    rt_exec_tasks.append(self.pop_queue())
 
             # for active rt tasks (1 quantum 실행)
-            for exec_task in exec_tasks:
-                exec_task.mode = 'O'
-                exec_task.exec_active()
+            for rt_exec_task in rt_exec_tasks:
+                rt_exec_task.exec_active(self.processor,self.memories)
 
+            # for other idle tasks (전력 소모 계산 및 1초 흐르기)
+            for i in range(len(self.rt_queue)):
+                task = self.rt_queue[i][1]
+                task.exec_idle(self.processor,self.memories)
+
+
+            for tup in self.rt_wait_queue:
+                tup[1].exec_idle(self.processor,self.memories)
+
+            self.add_utilization()
 
             # TODO 실행된 rt-task의 주기 끝났는지 확인해서 끝났으면 초기화 시키고 wait으로
 
